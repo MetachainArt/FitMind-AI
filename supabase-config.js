428e8c4838ad8ed8f6a51711ebe20a1e6f35c3ed
reedodev <reedo.dev@gmail.com>
2026-02-09 12:27:02 +0900

    Configure Supabase project URL and publishable key

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
-window.FITMIND_SUPABASE_URL = "";
-window.FITMIND_SUPABASE_ANON_KEY = "";
-window.FITMIND_SUPABASE_REDIRECT_URL = "";
+window.FITMIND_SUPABASE_URL = "https://cakuasqjzrqdghgpywis.supabase.co";
+window.FITMIND_SUPABASE_ANON_KEY = "sb_publishable_UGtTW7BQ66-6uPHzPuRs2A_vHYVYsM1";
+window.FITMIND_SUPABASE_REDIRECT_URL = "https://fitmind.dmssolution.co.kr";
